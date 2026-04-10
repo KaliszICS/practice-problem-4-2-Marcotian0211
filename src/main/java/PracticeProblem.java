221880e932a2455e66f390115816196e62bd10a0
Marcotian0211 <350327743@tdsb.ca>
2026-04-10 23:15:31 +0000

    update practice problem

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,35 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static String repeatLetter(char word,int number){
+		String word1="";
+		for(int i=0;i<number;i++)
+			word1+=word;
+         return(word1);
+	}
+	public static Integer countLetter(String word1,char word){
+		int count=0;
+		for(int i=0;i<word1.length();i++){
+			if(word1.charAt(i)==word){
+				count++;
+		}
+	}
+	return count;
+		
+	}
+	public static Integer evenSum(int num,int num1){
+		int count=0;
+		int min=Math.min(num,num1);
+		int max=Math.max(num,num1);
+		for(int i=min;i<=max;i++){
+			if(i%2==0){
+            count+=i;
+			}
+		}
+		return (count);
+		}
 
-}
+
+	}
+
+
